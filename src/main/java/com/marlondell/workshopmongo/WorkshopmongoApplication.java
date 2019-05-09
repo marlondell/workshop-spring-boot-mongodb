a023de0a4026c873e08ca37358d676ebc256e2f1
marlondell <marlondellaliberah@outlook.com>
2019-05-09 15:44:21 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/marlondell/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.marlondell.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marlondell/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.marlondell.workshopmongo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WorkshopmongoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
